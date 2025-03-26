b1ef72f3a7861cbcc04d259f57858ec6386f2ddc
Casey Goldstein <40779835+gekyume@users.noreply.github.com>
2025-03-25 23:38:56 -0400

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketMap from './MarketMap';
+
+function App() {
+  return (
+    <div className="App">
+      <MarketMap />
+    </div>
+  );
+}
+
+export default App;
